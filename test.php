43667f03f6415952c829c370e4fea147e9b9d769
canozokur <canozokur@users.noreply.github.com>
2015-12-19 20:41:38 +0200

    Incorporated colors from https://personal.sron.nl/~pault/

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+// <?php = .punctuation.section.embedded > darken(@red, 10%)
+
+/* Comments > @light-gray */
+
+// function = .storage > @purple
+// function_name = .entity.name.function > @blue
+function function_name(){
+
+  // return = .keyword.control > @purple
+  // array = .support.function > @cyan
+  // string = .string > @green
+  return array('string' => 'string');
+
+}
+
+// $ = .punctuation.definition.variable > @very-light-gray
+// var_name = .variable > @red
+// new = .keyword > @purple
+// SUPPORT_CLASS = .support.class > @light-orange
+// true = .constant > @orange
+$var_name = new SUPPORT_CLASS( true );
+
+?>
